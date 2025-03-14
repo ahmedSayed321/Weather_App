a413ba03e8852d7f1dd67df2fd7416c255e58305
Ahmed Sayed <ahmedsayeed233@gmail.com>
2025-03-14 17:36:56 +0200

    finish api services & home & search ui & passing data

---
+++ lib/ui/search_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app_project/models/WeatherResponse.dart';
+import 'package:weather_app_project/providers/weather_provider.dart';
+import 'package:weather_app_project/services/api_services.dart';
+import 'package:weather_app_project/ui/home_screen.dart';
+
+class SearchScreen extends StatelessWidget {
+  static const String routeName = "Search_Screen";
+  ApiServices apiServices = ApiServices();
+  SearchScreen({super.key, this.cityName});
+  String? cityName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.green),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            keyboardType: TextInputType.text,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 22,
+                horizontal: 15,
+              ),
+              suffixIcon: IconButton(
+                onPressed: () async {},
+                color: Colors.green,
+                icon: Icon(Icons.search),
+              ),
+              border: OutlineInputBorder(
+                //borderRadius: BorderRadius.circular(18),
+                borderSide: BorderSide(color: Colors.green, width: 2),
+              ),
+              hintText: "Enter your city",
+              hintStyle: TextStyle(color: Colors.black),
+              label: Text("Search"),
+              labelStyle: TextStyle(color: Colors.green, fontSize: 18),
+            ),
+            onSubmitted: (data) async {
+              cityName = data;
+              WeatherResponse response = await apiServices.getWeather(
+                cityName: cityName!,
+              );
+              Provider.of<WeatherProvider>(context, listen: false)
+                  .weatherResponse = response;
+              print(response.toJson());
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
